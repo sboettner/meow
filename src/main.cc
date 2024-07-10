2f1c6de0266334dd756c1dd926abb470a8a8d368
Stefan T. Boettner <116445372+sboettner@users.noreply.github.com>
2024-07-10 13:28:20 +0200

    note detection by viterbi algorithm

--- src/main.cc
+++ src/main.cc
@@ -12,9 +12,43 @@ T sqr(T x)
 }
 
 
+template<typename T>
+class Array2D {
+    T*  data;
+    int ni;
+    int nj;
+
+public:
+    Array2D(int ni, int nj):ni(ni), nj(nj)
+    {
+        data=new T[ni*nj];
+    }
+
+    ~Array2D()
+    {
+        delete[] data;
+    }
+
+    T& operator()(int i, int j)
+    {
+        assert(0<=i && i<ni);
+        assert(0<=j && j<nj);
+        return data[i*nj + j];
+    }
+};
+
+
+// analysis frame
 struct Frame {
     double  position;
     float   period;     // zero if unvoiced
+    float   pitch;
+};
+
+
+struct Chunk {
+    int     begin;
+    int     end;
     float   pitch;
 };
 
@@ -87,7 +121,7 @@ std::vector<Frame> compute_frame_decomposition(const Waveform& wave, int blocksi
                 float b=(normalized[i+1]-normalized[i-1])/2;
 
                 float peakval=normalized[i] - b*b/a/4;
-                if (peakval>bestpeakval) {
+                if (peakval>bestpeakval + 0.01f) {
                     bestperiod=i - b/a/2;
                     bestpeakval=peakval;
                 }
@@ -100,10 +134,10 @@ std::vector<Frame> compute_frame_decomposition(const Waveform& wave, int blocksi
             position=offs + blocksize/4;
         }
         else {
-            printf("\e[32;1mperiod=%.1f  freq=%.1f  val=%.4f\n", bestperiod, wave.get_samplerate()/bestperiod, bestpeakval);
-
             float freq=wave.get_samplerate() / bestperiod;
             float pitch=logf(freq / 440.0f) / M_LN2 * 12.0f + 69.0f;
+
+            printf("\e[32;1mperiod=%.1f  freq=%.1f  val=%.4f  pitch=%.2f\n", bestperiod, wave.get_samplerate()/bestperiod, bestpeakval, pitch);
 
             frames.push_back({ position, bestperiod, pitch });
             position+=bestperiod;
@@ -116,12 +150,85 @@ std::vector<Frame> compute_frame_decomposition(const Waveform& wave, int blocksi
 }
 
 
+void detect_notes(const std::vector<Frame>& frames)
+{
+    const int n=frames.size();
+
+    struct Node {
+        int     pitch;
+        int     back;
+        float   cost;
+    };
+
+    Array2D<Node> nodes(n, 5);
+
+    for (int j=0;j<5;j++) {
+        nodes(0, j).pitch=-1;
+        nodes(0, j).back=-1;
+        nodes(0, j).cost=0.0f;
+    }
+
+    for (int i=1;i<n;i++) {
+        if (frames[i].pitch>0) {
+            int p=lrintf(frames[i].pitch) - 2;
+
+            for (int j=0;j<5;j++, p++) {
+                float bestcost=INFINITY;
+                int bestback=0;
+
+                for (int k=0;k<5;k++) {
+                    float cost=nodes(i-1, k).cost;
+
+                    if (nodes(i-1, k).pitch!=p)
+                        cost+=10.0f / abs(nodes(i-1, k).pitch-p); // change penalty
+                    
+                    cost+=sqr(frames[i].pitch - p);
+
+                    if (cost<bestcost) {
+                        bestcost=cost;
+                        bestback=k;
+                    }
+                }
+
+                nodes(i, j).pitch=p;
+                nodes(i, j).back=bestback;
+                nodes(i, j).cost=bestcost;
+            }
+        }
+        else {
+            for (int j=0;j<5;j++) {
+                nodes(i, j).pitch=nodes(i-1, j).pitch;
+                nodes(i, j).back=j;
+                nodes(i, j).cost=nodes(i-1, j).cost;
+            }
+        }
+    }
+
+    int i=n-1, j=0;
+    for (int k=1;k<5;k++)
+        if (nodes(i, k).cost < nodes(i, j).cost)
+            j=k;
+
+    printf("total cost: %f\n", nodes(i, j).cost);
+    
+    static const char* notenames[]={ "C", "C#", "D", "Eb", "E", "F", "F#", "G", "G#", "A", "Bb", "B" };
+
+    while (i>=0) {
+        if (frames[i].pitch>0 && nodes(i, j).pitch>=0)
+            printf("i=%d  %s-%d %+.1f\n", i, notenames[nodes(i, j).pitch%12], nodes(i, j).pitch/12, 100.0f*(frames[i].pitch-nodes(i, j).pitch));
+
+        j=nodes(i--, j).back;
+    }
+}
+
+
 int main()
 {
     Waveform* wave=Waveform::load("testdata/example2.wav");
 
     auto frames=compute_frame_decomposition(*wave, 1024, 24);
-    printf("%ld frames\n", frames.size());
+    
+    detect_notes(frames);
 
     return 0;
 }
